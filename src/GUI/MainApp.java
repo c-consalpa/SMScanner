7d77499c3fe9661172ae9b9a5015d650ea63ea77
c-consalpa <konstantin.alpashkin@gmail.com>
2018-02-08 01:10:08 +0300

    checkbox and verionvalue read from ui

--- src/GUI/MainApp.java
+++ src/GUI/MainApp.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import netUtils.netBrowser;
@@ -15,7 +16,7 @@ import java.io.IOException;
 public class MainApp extends Application {
     private Stage primaryStage;
     private MainAppController controller;
-    AnchorPane root;
+    SplitPane root;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -26,17 +27,18 @@ public class MainApp extends Application {
     private void initLayout() throws IOException {
         primaryStage.setTitle("Hello World");
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("mainScreen.fxml"));
+        loader.setLocation(getClass().getResource("SMScanner.fxml"));
         this.root = loader.load();
+
         controller = loader.getController();
+
         controller.setMainApp(this);
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
-
+        launch(args);
     }
 
 
--- src/GUI/MainAppController.java
+++ src/GUI/MainAppController.java
@@ -1,43 +1,83 @@
 package GUI;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
 
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
 import netUtils.PullTask;
 import netUtils.Scheduler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by c-consalpa on 2/1/2018.
  */
 public class MainAppController {
     private MainApp mainApp;
-
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
     }
 
     @FXML
-    private Button startBtn;
+    private CheckBox prod_EAM;
 
     @FXML
-    private TextArea consoleTextArea;
+    private CheckBox prod_XES;
+
+    @FXML
+    private CheckBox prod_XEC;
+
+    @FXML
+    private CheckBox prod_SM;
+
+    @FXML
+    private CheckBox prod;
+
+    @FXML
+    private GridPane prod_checkBoxes;
+
+    @FXML
+    private ChoiceBox<String> choice_version;
+
 
     @FXML
     private void initialize() {
-        System.out.println("t");
-
+        setupChoiceList();
     }
 
     @FXML
-    private void startPolling() {
-        Scheduler scheduler = new Scheduler(new String[]{"XEServer"}, "9.0.0", 10000);
-
-        System.out.println("qqq");
+    private void onStartBtn(ActionEvent ev) {
+        if (getProducts().size()==0) {
+            return;
+        }
+        System.out.println(getVersion());
+        System.out.println(getProducts());
     }
 
 
+    private void setupChoiceList() {
+        choice_version.setItems(FXCollections.observableArrayList("a", "b"));
+    }
 
+    private List<String> getProducts() {
+        List<String> l = new ArrayList<>(5);
+        for (Object node:
+                prod_checkBoxes.getChildren()) {
+            if  (node instanceof CheckBox && ((CheckBox) node).isSelected()) {
+                l.add(((CheckBox) node).getText());
+            }
+        }
+        return l;
+    }
+
+    private String getVersion() {
+        return choice_version.getValue();
+    }
 }
